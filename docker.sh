5af4678d9e6ff5bfc44ae2d2ec01276d4df93828
burmist-git <burm.leonid@gmail.com>
2022-11-27 21:02:22 +0100

    Add run simtelarray

--- docker.sh
+++ docker.sh
@@ -4,12 +4,28 @@ function docker_build_sh {
     time docker build -f Dockerfile -t simtelarray $PWD
 }
 
+function simtelarray_build_sif {
+    time singularity build simtelarray.sif docker-daemon://simtelarray:latest
+}
+
+function singularity_run_simtelarray {
+    singularity run -B /home/burmist/home2/work/CTA/corsika_reader_my/tmp/data/:/store/cta/cta03/ simtelarray.sif /run_simtelarray/run_simtelarray.sh -test_sim_telarray
+}
+
+function clean {
+    rm -rf simple_test_sim_telarray.log
+    rm -rf telarray_rand.conf.used*
+}
+
 function printHelp {
     echo " --> ERROR in input arguments"
-    echo " -h    : print help"
-    echo " -b    : create docker image"
-    echo " --li  : list images"
-    echo " --lc  : list containers"
+    echo " -h        : print help"
+    echo " -b        : create docker image"
+    echo " -s        : singularity .sif file"
+    echo " -c        : clean"
+    echo " -rungamma : singularity run gamma"
+    echo " --li      : list images"
+    echo " --lc      : list containers"
 }
 
 if [ $# -eq 0 ]; then
@@ -19,6 +35,12 @@ else
 	printHelp
     elif [ "$1" = "-b" ]; then
 	docker_build_sh
+    elif [ "$1" = "-s" ]; then
+	simtelarray_build_sif
+    elif [ "$1" = "-rungamma" ]; then
+	singularity_run_simtelarray
+    elif [ "$1" = "-c" ]; then
+	clean
     elif [ "$1" = "--li" ]; then
 	docker images
     elif [ "$1" = "--li" ]; then
--- run_simtelarray/run_simtelarray.sh
+++ run_simtelarray/run_simtelarray.sh
@@ -8,8 +8,15 @@ function printHelp {
     echo " [0] -h                        : print help"
 }
 
+#
 sim_telarray_dir="/simtelarray/"
 cfg_dir="/run_simtelarray/cfg/"
+#
+in_corsika_file="/store/cta/cta03/corsika_data/gamma/corsika_run307.corsika.gz"
+out_simtel_file="/store/cta/cta03/simtel_data/gamma/data/corsika_run307.simtel.gz"
+out_hist_file="/store/cta/cta03/simtel_data/gamma/hdata/corsika_run307.hdata"
+out_log_file="/store/cta/cta03/simtel_data/gamma/log/corsika_run307.log"
+#
 
 if [ $# -eq 0 ] 
 then    
@@ -21,16 +28,12 @@ else
 	$sim_telarray_dir/sim_telarray/bin/sim_telarray | tee simple_test_sim_telarray.log
     elif [ "$1" = "-test_sim_telarray" ]; then
 	echo "-test_sim_telarray"
-	#in_corsika_file="/home/burmist/home2/work/CTA/corsika_reader_my/tmp/data/gamma/corsika_run307.corsika.gz"
-	#out_hist_file="/home/burmist/home2/work/CTA/corsika_reader_my/tmp/data/gamma/corsika_run307.hdata"
-	#out_simtel_file="/home/burmist/home2/work/CTA/corsika_reader_my/tmp/data/gamma/corsika_run307.simtel.gz"
-	#out_log_file="/home/burmist/home2/work/CTA/corsika_reader_my/tmp/data/gamma/corsika_run307.log"
 	#
-	#rm -rf $out_hist_file
-	#rm -rf $out_simtel_file
-	#rm -rf $out_log_file
+	rm -rf $out_hist_file
+	rm -rf $out_simtel_file
+	rm -rf $out_log_file
 	#
-	#$sim_telarray_dir/sim_telarray/bin/sim_telarray -I$cfg_dir -c $cfg_dir/CTA-PROD5-LaPalma-baseline_4LSTs_MAGIC.cfg -DNUM_TELESCOPES=1 -DNO_STEREO_TRIGGER=1 -C min_photons=0 -C min_photoelectrons=1 -C save_photons=3 -C only_triggered_telescopes=1 -C only_triggered_arrays=1 -C random_state=auto -C show=all -C maximum_telescopes=1 -C fadc_sum_bins=75 -C telescope_phi=180 -C telescope_zenith_angle=20 -C asum_threshold=8.25 -C trigger_current_limit=2000.0 -C nightsky_background=all:0.386 -C nsb_scaling_factor=1 -C dark_events=0 -C pedestal_events=0 -h $out_hist_file -o $out_simtel_file $in_corsika_file 2>&1 > $out_log_file
+	$sim_telarray_dir/sim_telarray/bin/sim_telarray -I$cfg_dir -c $cfg_dir/CTA-PROD5-LaPalma-baseline_4LSTs_MAGIC.cfg -DNUM_TELESCOPES=1 -DNO_STEREO_TRIGGER=1 -C min_photons=0 -C min_photoelectrons=1 -C save_photons=3 -C only_triggered_telescopes=1 -C only_triggered_arrays=1 -C random_state=auto -C show=all -C maximum_telescopes=1 -C fadc_sum_bins=75 -C telescope_phi=180 -C telescope_zenith_angle=20 -C asum_threshold=8.25 -C trigger_current_limit=2000.0 -C nightsky_background=all:0.386 -C nsb_scaling_factor=1 -C dark_events=0 -C pedestal_events=0 -h $out_hist_file -o $out_simtel_file $in_corsika_file 2>&1 > $out_log_file
 	#
 	#echo "out_log_file ---> $out_log_file"
     elif [ "$1" = "-h" ]; then
